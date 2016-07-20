853772efdba462ce788f5b9f5405c16f33a9f0fa
Neeraj Gupta <neerjaj2@gmail.com>
2016-07-20 10:47:53 +0530

    Update update_ecs_agent.sh

--- update_ecs_agent.sh
+++ update_ecs_agent.sh
@@ -7,12 +7,10 @@ CLUSTERS=$(aws ecs list-clusters --output text --region $REGION | awk '{print $2
 
 for cluster in $CLUSTERS
 do
-	echo $cluster
 	CONTAINER_INSTANCES=$(aws ecs list-container-instances --cluster $cluster | awk '{print $2}')
 	for container in $CONTAINER_INSTANCES
 	do
 		CURRENT_VERSION=$(aws ecs describe-container-instances --container-instance $container --cluste $cluster --region $REGION --output text --query 'containerInstances[].versionInfo[].agentVersion')
-#		aws ecs update-container-agent --cluster $CLUSTER_NAME --container-instance $container
-		echo $container $CURRENT_VERSION 
+		aws ecs update-container-agent --cluster $CLUSTER_NAME --container-instance $container
 	done
 done
